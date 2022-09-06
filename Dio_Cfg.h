a0349404212ba02d753ad410f2601b3887451f08
Es-saidi Zakaria <zakaria95essaidi@gmail.com>
2022-09-06 15:57:06 +0100

    Add files via upload

---
+++ Dio_Cfg.h
@@ -0,0 +1,27 @@
+#ifndef DIO_CFG_H
+#define DIO_CFG_H
+
+
+#define HOW_MANY_CONFIGURED_CHANNELS    (2U)
+
+#define HOW_MANY_CONFIGURED_PORTS       (1U)
+
+#define HOW_MANY_CONFIGURED_GROUPS      (2U)
+
+#define BUTTON_PORT_00_PIN_7            (0U)
+#define LED_PORT_00_PIN_5               (1U)
+
+#define PORT_00                         (0U)
+
+#define PORT_00_ADDRESS                 (0xF003A000)
+
+
+#define PXX_IN_OFFSET                   (0x00000024)
+#define PXX_OMR_OFFSET                  (0x00000004)
+#define PXX_IOCR4_OFFSET                (0x00000014)
+
+#define PIN_5                           (5U)
+#define PIN_7                           (7U)
+
+
+#endif /*DIO_CFG_H*/
